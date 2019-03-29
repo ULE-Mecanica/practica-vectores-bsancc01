fd967df0e65e6655cc0a3209b855064015a971c9
bsancc01 <47888266+bsancc01@users.noreply.github.com>
2019-03-29 10:24:00 +0100

    Add files via upload

---
+++ comparavectores.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define TAM 5
+void cargar(int v[5]);
+int compara(int v1[5], int v2[5]);
+int main () {
+int v1=(1, 2, 3, 4, 5);
+int v2
+printf("dame los 5 valores: \n", v2);
+scanf("%d", &v2[5]);
+cargar (v1);
+cargar (v2);
+}
+void cargar (int v[5]) {
+int i;
+for (i=0; i<TAM; i++) {
+scanf("%d", &v[i]);
+}
+}
+int compara (int v[5]) {
+if (compara (v1[5], v2[5])) {
+printf("los vectores son iguales \n");
+} else {
+if (printf("los vectores no son iguales \n"));
+return compara;
+}
+}
